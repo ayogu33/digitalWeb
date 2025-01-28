57473339aa6f5808785967b210c5a52bf0063763
ayogu33 <ayoguchibuezekingsley1@gmail.com>
2025-01-28 01:44:32 -0800

    any message

---
+++ index.js
@@ -0,0 +1,158 @@
+// console.log('turn 2 fan!')
+// console.log('turn 2 fan!')
+// console.log('turn 2 fan!')
+// console.log('turn 2 fan!')
+// console.log('turn 2 fan!')
+// console.log('turn 2 fan!')
+
+// var message = 'turn our fan'
+// let num =  2 
+// const add = num + 2
+// const mult = num + 3
+// const sub = 12 - num
+
+
+// console.log(num)
+// console.log(add)
+// console.log(mult)
+// console.log(sub)
+
+// num = 5
+
+// console.log(message)
+// console.log(message)
+// console.log(message)
+// console.log(message)
+// console.log(message)
+// console.log(message)
+
+//let power = 10 ** 3
+// console.log(power)
+
+// let remender = 10 / 2
+// console.log(remender)
+
+// let can = 10 % 2
+// console.log(can)
+
+// let number = 5
+// let number2 =10
+
+// num = num + 5
+// console.log('updated num', num)
+
+// let firstname = 'Obi '
+// let lastname = 'peter'
+
+// let fullname =  firstname + '' + lastname
+// let name = firstname + lastname
+// console.log(name)
+
+// let num1 = '4'
+// let num2 = '10' 
+// let num3 = 10 
+
+
+// console.log(num1 * num2 / num3)
+
+// function showmessages(){
+//     console.log(message)
+//     console.log(message)
+//     console.log(message)  
+// }
+
+// showmessages()
+// showmessages()
+// showmessages()
+
+// function addNums(){
+//     let num1 = 5 
+//     let num2 = 10 
+//     console.log('the result will show below')
+//     console.log(num1 + num2)
+// }
+// ayogu33
+// addNums()
+// addNums()
+// addNums()
+// addNums()
+// addNums()
+// addNums()
+
+
+// function addnumber(num1, num2, explaner){
+//     console.log(explaner)
+//     console.log(num1 + num2)
+
+// }
+// addnumber(50, 10, 'this is my result')
+// addnumber(50, 10, 'see my result below')
+// addnumber(50, 100, 'the result is below')
+
+// function addMull(num1, num2, str){
+//     let add = num1 + num2
+//     let result= add + 10 
+//     let output = str + ' ' + result
+//     return output
+// }
+// let result = addMull(10, 10, 'The result is')
+// // console.log(result)
+
+// let car1 = {Name: 'Camery', Model: 2024, Tyer: 4, color: 'black'}
+// let car2 = {Name: 'Ikenga', Model: 2023, Tyer: 4, color: 'black'}
+
+// //console.log(car1)
+
+
+// let car3 = {}
+// car3.Name = 'Toyota'
+// car3.model = 2023
+// car3.tyer = 4
+// car3.color = 'sty blue'
+// car3.price ='%20000'
+
+// //console.log(car3)
+// let value = Object.value(car)
+// let 
+
+// let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZSJOSHFUIS"
+// let sentence = 'i will be done very soon'
+// let position = sentence.at(-4)
+// console.log(position)
+
+// console.log(str.slice (5, 21))
+// console.log(str.substring (5, 21))
+// console.log(str.substr (5, 21))
+
+// let numl = 200
+// let string = `he said "Hello how are you" I'm coming ${sentence} ${numlt} ${str}`
+// console.log(string)
+// let car = {Name: 'Land-cruser'}
+// console.log(car)
+
+
+let numil= 200 
+let numstr = numil.toString()
+console.log(numstr)
+let decimolnum = 10.2989677750
+let decimal = decimolnum.toFixed(2)
+console.log(Number(decimal) + 30)
+
+let myarray = ['Camry', 'Blue', 4, 2024, 20000]
+console.log(myarray)
+console.log(myarray[0])
+console.log(myarray[1])
+myarray.push('Toyota')
+//myarray.unshift(red)
+myarray.push(2020)
+myarray.push(22000)
+console.log(myarray)
+myarray.pop()
+myarray.shift()
+console.log(myarray)
+
+myarray[0] = 'Mercedics'
+myarray[1] = 'blsck'
+myarray[2] = 2029
+
+console.log(myarray)
