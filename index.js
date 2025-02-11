c90992ec447fab0b5afb00b0865df09d3cabee01
ayogu33 <ayoguchibuezekingsley1@gmail.com>
2025-02-11 03:35:52 -0800

    another

--- index.js
+++ index.js
@@ -307,3 +307,26 @@ myset.add('Bank')
 myset.add('Black')
 
 console.log(myset)
+
+// try {
+//     adddlert("Welcome guest!");
+//   }
+//   catch(err) {
+//     document.getElementById("demo").innerHTML = err.message;
+//   }
+
+function myFunction() {
+    const message = document.getElementById("p01");
+    message.innerHTML = "";
+    let x = document.getElementById("demo").value;
+    try {
+      if(x.trim() == "") throw "empty";
+      if(isNaN(x)) throw "not a number";
+      x = Number(x);
+      if(x < 5) throw "too low";
+      if(x > 10) throw "too high";
+    }
+    catch(err) {
+      message.innerHTML = "Input is " + err;
+    }
+  }
