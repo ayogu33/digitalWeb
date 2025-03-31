150fecdd24fe3e45fe3ca731d875cc5c5a798131
ayogu33 <ayoguchibuezekingsley1@gmail.com>
2025-03-31 06:23:08 -0700

    my new form validation

--- index.js
+++ index.js
@@ -330,3 +330,57 @@ function myFunction() {
       message.innerHTML = "Input is " + err;
     }
   }
+
+
+
+
+  const form = document.getElementById('form')
+  const username = document.getElementById('username')
+  const email = document.getElementById('email')
+  const password = document.getElementById('password')
+  const password2 = document.getElementById('password2')
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateinputs();
+  });
+
+  const setError = (element, message) => {
+    const inputcontrol = element.parentElement;
+    const errorDisplay = inputcontrol.queryselector('.error')
+
+    errorDisplay.innerText = message;
+    inputcontrol.classList.add('error');
+    inputcontrol.classList.remove('success');
+  }
+
+  const setSuccess = element => {
+    const inputcontrol = element.parentElement;
+    const errorDisplay = inputcontrol.queryselector('.error')
+
+    errorDisplay.innerText = '';
+    inputcontrol.classList.add('error');
+    inputcontrol.classList.remove('success');
+  }
+
+  const isvalidEmail = email {
+  }
+
+  const validateinputs = () => {
+    const usernamevalue = username.value.trim();
+    const emailvalue = email.value.trim();
+    const passwordvalue =password.value.trim();
+    const password2value =password2.value.trim();
+
+    if(username === '') {
+        setError(username, 'username is required')
+    } else {
+        setSuccess(username)
+    }
+
+    if(emailvalue === '') {
+        setError(email, 'Email is required');
+
+    }
+  };
